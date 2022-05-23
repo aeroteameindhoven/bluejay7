6e50c7299e45e5baa6e0232bde917f034361cc0d
alactro <70527101+alactro@users.noreply.github.com>
2022-05-23 12:33:22 +0200

    vision package added

---
+++ src/bluejay_vision/include/bluejay_vision/vision.h
@@ -0,0 +1,14 @@
+#ifndef VISION_H
+#define VISION_H
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Path.h>
+
+#endif // VISION_H
---
+++ src/bluejay_vision/src/vision.cpp
@@ -0,0 +1,10 @@
+#include "math.h"
+#include "bluejay_vision/vision.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "vision");
+    ros::spin();
+
+    return 0;
+}
